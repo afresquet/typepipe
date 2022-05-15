b12a2a471fe42d00c2a35d33293e72b456e01189
Alvaro Fresquet <alvarofresquet@gmail.com>
2022-05-15 20:43:17 +0200

    moved steps main import

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,5 @@
 import Pipeline from "./classes/Pipeline";
 export default Pipeline;
 export { default as Match } from "./classes/Match";
-// Steps
-export * from "./steps/assert";
-export * from "./steps/ifelse";
-export * from "./steps/match";
-export * from "./steps/pairwise";
-export * from "./steps/tap";
 // Types
 export type { TypePipe } from "./types/TypePipe";
