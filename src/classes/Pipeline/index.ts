51ee0a5c112d1d9f8e6edccb99263a31320f38be
Alvaro Fresquet <alvarofresquet@gmail.com>
2022-05-18 21:57:34 +0200

    added property to hold the context to Pipeline

--- src/classes/Pipeline/index.ts
+++ src/classes/Pipeline/index.ts
@@ -53,6 +53,8 @@ export default class Pipeline<
 		>;
 	}
 
+	private currentContext: any;
+
 	/**
 	 * Changes the context passed to the following functions of the pipeline.
 	 *
@@ -176,18 +178,22 @@ export default class Pipeline<
 		Global
 	> {
 		const composition = this.functions.reduce(
-			(fn1, fn2) => (value, context, global) => {
+			(fn1, fn2) => (value, _, global) => {
 				const handleResult = (res: any) => {
 					if (res instanceof ChangeContext) {
-						const context = res.run();
+						const ctx = res.run();
 
-						if (isPromise(context)) {
-							return context.then(c => {
-								return fn2(res.value, c as any, global);
+						if (isPromise(ctx)) {
+							return ctx.then(c => {
+								this.currentContext = c;
+
+								return fn2(res.value, c, global);
 							});
 						}
 
-						return fn2(res.value, context, global);
+						this.currentContext = ctx;
+
+						return fn2(res.value, ctx, global);
 					}
 
 					if (res instanceof ErrorHandler) {
@@ -195,13 +201,13 @@ export default class Pipeline<
 
 						this.errorHandler = errorHandler;
 
-						return fn2(res.value, context, global);
+						return fn2(res.value, this.currentContext, global);
 					}
 
-					return fn2(res, context, global);
+					return fn2(res, this.currentContext, global);
 				};
 
-				const result = fn1(value, context, global);
+				const result = fn1(value, this.currentContext, global);
 
 				if (isPromise(result)) {
 					return result.then(handleResult);
@@ -214,6 +220,9 @@ export default class Pipeline<
 		return (value, context, global) => {
 			// Reset the error handler
 			this.errorHandler = undefined;
+
+			// Set the context
+			this.currentContext = context;
 
 			const onError = (error: unknown) => {
 				if (!this.errorHandler) {
